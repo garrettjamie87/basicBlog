64eabe94ee5e3c900df1ae192dd15d5758d0343b
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-10 16:02:30 +0100

    add map() to req.params.postName

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { forEach } = require("lodash");
+const {
+  forEach
+} = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -28,8 +30,7 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
-    posts: postArr
-
+    posts: postArr,
   });
 });
 
@@ -51,18 +52,27 @@ app.get("/compose", function (req, res) {
 
 app.post("/compose", function (req, res) {
   const titulo = req.body.postTitle;
-  const cuerpo = req.body.postBody 
+  const cuerpo = req.body.postBody;
   const post = {
-   title: titulo,
-   body: cuerpo
-  }
-  
+    title: titulo,
+    body: cuerpo,
+  };
+
   postArr.push(post);
-  res.redirect('/');
- 
+  res.redirect("/");
 });
 
-
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = req.params.postName;
+  postArr.map((x)=>{
+    if(requestedTitle === x.title){
+      console.log("match found")
+    } else {
+      console.log("no match found")
+    }
+  })
+  
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
